4c34657ae91e20a7743b6ac618f81f1f1b64e9fe
RidhoAkhsan <akhsanridho23@gmail.com>
2024-03-06 21:45:22 +0700

    navbar with dropdown

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let lastScrollTop = 0;
 let isAtTop = true;
 
-window.addEventListener("scroll", function () {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener(
+  "scroll",
+  function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollTop > lastScrollTop && scrollTop > 50) {
-    document.querySelector("nav").classList.add("scrolled");
-    isAtTop = false;
-  } else if (scrollTop === 0 && !isAtTop) {
-    document.querySelector("nav").classList.remove("scrolled");
-    isAtTop = true;
-  }
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-}, false);
+    if (scrollTop > lastScrollTop && scrollTop > 50) {
+      // Scroll down and not at the top
+      document.querySelector("nav").classList.add("scrolled");
+      isAtTop = false;
+    } else if (scrollTop === 0 && !isAtTop) {
+      // Scroll to top
+      document.querySelector("nav").classList.remove("scrolled");
+      isAtTop = true;
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  },
+  false
+);
